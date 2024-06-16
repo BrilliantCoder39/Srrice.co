1fa973f0af4719e842617971d11183947d213189
BrilliantCoder39 <bh1868159@gmail.com>
2024-06-16 12:43:17 -0700

    Update CustomGoogleMaps.js

--- src/components/GoogleMaps/CustomGoogleMaps.js
+++ src/components/GoogleMaps/CustomGoogleMaps.js
@@ -46,7 +46,7 @@ const Map = () => {
   return (
     <div style={containerStyle}> {/* Parent container */}
       <NavBar projects={projects} onProjectClick={handleProjectClick} /> {/* NavBar */}
-      <LoadScript googleMapsApiKey="AIzaSyBuL4FuumNhtCmkLqHMlVlHRSTbKMeTLvU">
+      <LoadScript googleMapsApiKey="AIzaSyCSBAB1S5My3mka9nbi1DgAvK6TaeV1bAc">
         <GoogleMap
           mapContainerStyle={{ flex: 1 }} // GoogleMap takes up remaining space
           center={defaultCenter}
